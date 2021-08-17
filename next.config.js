9ca5653e5afc0b4e7341ad2502be550b577dbb11
Apichaya051241 <B6014704@g.sut.ac.th>
2021-08-17 17:51:36 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
   reactStrictMode: true,
-  images: {domains: ["lh3.googleusercontent"]},
-  // images: {domains: ["lh3.googleusercontent"]},
+  images: {domains: ["/lh3.googleusercontent.com"]},
+  
 }
